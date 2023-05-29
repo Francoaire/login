9a7fa767d8479fee526abb89cd0570e1065fcff9
unknown <jeanfrancoaire@gmail.com>
2023-05-29 06:34:55 +0200

    first commit

---
+++ firebaseconfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig ={
+    
+        apiKey: "AIzaSyCrIH-z1t_Vjo5yZ-61TEUtQTwMnrzrZ8U",
+        authDomain: "bslogin-fe8ac.firebaseapp.com",
+        projectId: "bslogin-fe8ac",
+        storageBucket: "bslogin-fe8ac.appspot.com",
+        messagingSenderId: "972087238543",
+        appId: "1:972087238543:web:9c198f1772273fb2cef5fa",
+        measurementId: "G-65PC10KZT3"
+      
+
+};
+const app = initializeApp(firebaseConfig);
